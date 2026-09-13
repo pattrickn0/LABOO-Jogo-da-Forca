c71721bdb75a308f2808cc60950cb34f5a09d614
Pattrick de Barros Nunes <pattrickn0@gmail.com>
2026-09-13 20:21:05 -0300

    feat: inicializar projeto do jogo da forca com modelos de domínio, repositórios e configuração principal

--- src/br/edu/iff/jogoforca/Main.java
+++ src/br/edu/iff/jogoforca/Main.java
@@ -2,6 +2,7 @@ package br.edu.iff.jogoforca;
 
 import br.edu.iff.jogoforca.dominio.rodada.Rodada;
 import br.edu.iff.jogoforca.dominio.rodada.RodadaAppService;
+
 import java.util.Scanner;
 
 public class Main {
@@ -12,23 +13,53 @@ public class Main {
         try {
             // 2) Criar e inserir Temas
             br.edu.iff.bancodepalavras.dominio.tema.TemaFactory temaFactory = app.getTemaFactory();
-            br.edu.iff.bancodepalavras.dominio.tema.TemaRepository temaRepo = app.getRepositoryFactory().getTemaRepository();
-            
+            br.edu.iff.bancodepalavras.dominio.tema.TemaRepository temaRepo = app.getRepositoryFactory()
+                    .getTemaRepository();
+
             br.edu.iff.bancodepalavras.dominio.tema.Tema tema1 = temaFactory.getTema("Animais");
             br.edu.iff.bancodepalavras.dominio.tema.Tema tema2 = temaFactory.getTema("Frutas");
+            br.edu.iff.bancodepalavras.dominio.tema.Tema tema3 = temaFactory.getTema("Paises");
+            br.edu.iff.bancodepalavras.dominio.tema.Tema tema4 = temaFactory.getTema("Cores");
+            br.edu.iff.bancodepalavras.dominio.tema.Tema tema5 = temaFactory.getTema("Profissoes");
             temaRepo.inserir(tema1);
             temaRepo.inserir(tema2);
+            temaRepo.inserir(tema3);
+            temaRepo.inserir(tema4);
+            temaRepo.inserir(tema5);
 
             // 3) Criar/Inserir Palavras
-            br.edu.iff.bancodepalavras.dominio.palavra.PalavraAppService palavraService = br.edu.iff.bancodepalavras.dominio.palavra.PalavraAppService.getSoleInstance();
-            palavraService.novaPalavra("CACHORRO", tema1.getId());
-            palavraService.novaPalavra("GATO", tema1.getId());
-            palavraService.novaPalavra("MACA", tema2.getId());
-            palavraService.novaPalavra("BANANA", tema2.getId());
+            br.edu.iff.bancodepalavras.dominio.palavra.PalavraAppService palavraService = br.edu.iff.bancodepalavras.dominio.palavra.PalavraAppService
+                    .getSoleInstance();
+
+            String[] animais = { "CACHORRO", "GATO", "ELEFANTE", "GIRAFA", "LEÃO", "TIGRE", "ZEBRA", "MACACO", "CAVALO",
+                    "URSO" };
+            for (String p : animais)
+                palavraService.novaPalavra(p, tema1.getId());
+
+            String[] frutas = { "MAÇA", "BANANA", "LARANJA", "MORANGO", "UVA", "ABACAXI", "MELANCIA", "MANGA", "PERA",
+                    "KIWI" };
+            for (String p : frutas)
+                palavraService.novaPalavra(p, tema2.getId());
+
+            String[] paises = { "BRASIL", "ARGENTINA", "CANADA", "JAPAO", "ALEMANHA", "ITALIA", "ESPANHA", "MEXICO",
+                    "PORTUGAL", "FRANCA" };
+            for (String p : paises)
+                palavraService.novaPalavra(p, tema3.getId());
+
+            String[] cores = { "VERMELHO", "AZUL", "VERDE", "AMARELO", "ROXO", "LARANJA", "PRETO", "BRANCO", "MARROM",
+                    "ROSA" };
+            for (String p : cores)
+                palavraService.novaPalavra(p, tema4.getId());
+
+            String[] profissoes = { "MEDICO", "ENGENHEIRO", "PROFESSOR", "ADVOGADO", "PROGRAMADOR", "DESIGNER",
+                    "BOMBEIRO", "POLICIAL", "CANTOR", "ATOR" };
+            for (String p : profissoes)
+                palavraService.novaPalavra(p, tema5.getId());
 
             // 4) Criar e inserir Jogador
             br.edu.iff.jogoforca.dominio.jogador.JogadorFactory jogadorFactory = app.getJogadorFactory();
-            br.edu.iff.jogoforca.dominio.jogador.JogadorRepository jogadorRepo = app.getRepositoryFactory().getJogadorRepository();
+            br.edu.iff.jogoforca.dominio.jogador.JogadorRepository jogadorRepo = app.getRepositoryFactory()
+                    .getJogadorRepository();
             br.edu.iff.jogoforca.dominio.jogador.Jogador jogador = jogadorFactory.getJogador("Membro");
             jogadorRepo.inserir(jogador);
 
@@ -51,10 +82,10 @@ public class Main {
                 while (!rodada.encerrou()) {
                     System.out.println("\nErros: " + rodada.getQtdeErros() + "/" + Rodada.getMaxErros());
                     rodada.exibirBoneco(System.out);
-                    
+
                     System.out.println("\nPalavra(s):");
                     rodada.exibirItens(System.out);
-                    
+
                     System.out.println("\nLetras erradas: ");
                     rodada.exibirLetrasErradas(System.out);
 
@@ -71,7 +102,7 @@ public class Main {
                     } else if (entrada.length() == 1) {
                         rodada.tentar(entrada.charAt(0));
                     } else {
-                        System.out.println("Entrada inválida.");
+                        System.out.println("Entrada inválida. Digite uma letra ou 'arriscar'. ");
                     }
                 }
 
@@ -79,23 +110,24 @@ public class Main {
 
                 System.out.println("\n====== FIM DE JOGO ======");
                 rodada.exibirBoneco(System.out);
-                
+
                 System.out.println("\nPalavra(s):");
                 rodada.exibirPalavras(System.out);
-                
+
                 if (rodada.descobriu()) {
                     System.out.println("\nParabéns, você venceu! Pontos ganhos: " + rodada.calcularPontos());
                 } else {
-                    System.out.println("\nVocê perdeu... A(s) palavra(s) foram arriscadas incorretamente ou você atingiu o limite de erros.");
+                    System.out.println(
+                            "\nVocê perdeu... A(s) palavra(s) foram arriscadas incorretamente ou você atingiu o limite de erros.");
                 }
-                
+
                 System.out.println("Pontuação total do jogador: " + rodada.getJogador().getPontuacao());
-                
+
                 System.out.print("\nDeseja jogar novamente? (S/N): ");
                 String resposta = scanner.nextLine().trim().toUpperCase();
                 continuar = resposta.equals("S");
             }
-            
+
             scanner.close();
 
         } catch (Exception e) {
